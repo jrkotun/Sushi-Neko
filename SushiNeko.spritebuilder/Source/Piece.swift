a20d5fadd775bded3b816d91400ccf47470b5259
Habeeb Kotun Jr <hjrkotun@gmail.com>
2015-06-29 09:48:40 -0700

    Basic layout for game is created in Spritebuilder. Created the Character and Piece classes in Xcode and I'm about to start coding said classes.

---
+++ SushiNeko.spritebuilder/Source/Piece.swift
@@ -0,0 +1,13 @@
+//
+//  Piece.swift
+//  SushiNeko
+//
+//  Created by Habeeb Kotun Jr. on 6/29/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Piece: CCNode {
+   
+}
